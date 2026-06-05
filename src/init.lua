4f08e6306f40c9aa81953621a66db1d36bed7802
ricardojrgavin-afk <ricardojr.gavin@gmail.com>
2026-06-04 19:08:46 -0500

    Initialize BrainrotPolice script with import and path functions
    Added functions for importing objects and getting GitHub paths.

--- src/init.lua
+++ src/init.lua
@@ -1,1 +1,29 @@
+--[[
+For a more in depth explanation and rundown of this script, I recommend you check out https://github.com/ricardojrgavin-afk/BrainrotPolice
+]]
+
+local env = getgenv()
+
+if not isfolder("BrainrotPolice") then makefolder("BrainrotPolice") end
+
+function env.import(id)
+    return game:GetObjects(id)[1]
+end
+
+function env.getgitpath(where)
+    local mainBuild = "https://raw.githubusercontent.com/ricardojrgavin-afk/BrainrotPolice/refs/heads/main/"
+    if where == "src" then
+        return mainBuild .. "src/"
+    elseif where == "games" then
+        return mainBuild .. "src/games/"
+    end
+end
+
+game:GetService("GuiService").ErrorMessageChanged:Connect(function()
+    if env.autorjjjj then
+        game:GetService("TeleportService"):Teleport(game.PlaceId)
+    end
+end)
+
+loadstring(game:HttpGet(getgitpath("src").."ui.lua"))()
 
