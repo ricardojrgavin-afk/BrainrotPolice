b8e79ce4a9b7dda95444cbc69e2b0fcd65b1bbc6
ricardojrgavin-afk <ricardojr.gavin@gmail.com>
2026-06-23 03:21:05 -0500

    Enhance init.lua with config and error handling
    Added configuration management and error handling features.

--- src/init.lua
+++ src/init.lua
@@ -1,17 +1,23 @@
---[[
-For a more in depth explanation and rundown of this script, I recommend you check out https://github.com/ricardojrgavin-afk/BrainrotPolice
-]]
-
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 local env = getgenv()
 
 if not isfolder("BrainrotPolice") then makefolder("BrainrotPolice") end
+if not isfile("BrainrotPolice/Config.json") then
+    writefile("BrainrotPolice/Config.json", game:GetService("HttpService"):JSONEncode({
+        settings = {
+            auto_rejoin_on_kick = false,
+            disable_3d_rendering = false
+        }
+    }))
+end
 
 function env.import(id)
     return game:GetObjects(id)[1]
 end
 
 function env.getgitpath(where)
-    -- Cleaned up by removing /refs/heads/ and leaving it right at main/
     local mainBuild = "https://raw.githubusercontent.com/ricardojrgavin-afk/BrainrotPolice/main/"
     if where == "src" then
         return mainBuild .. "src/"
@@ -20,10 +26,22 @@ function env.getgitpath(where)
     end
 end
 
+function env.setconfig(key, value)
+    local httpservice = game:GetService("HttpService")
+    local dec = httpservice:JSONDecode(readfile("BrainrotPolice/Config.json"))
+    dec[tostring(game.PlaceId)] = dec[tostring(game.PlaceId)] or {}
+    dec[tostring(game.PlaceId)][key] = value
+    writefile("BrainrotPolice/Config.json", httpservice:JSONEncode(dec))
+end
+
 game:GetService("GuiService").ErrorMessageChanged:Connect(function()
     if env.autorjjjj then
         game:GetService("TeleportService"):Teleport(game.PlaceId)
     end
 end)
 
-loadstring(game:HttpGet(getgitpath("src").."ui.lua"))()
+loadstring(game:HttpGet(env.getgitpath("src").."ui.lua"))()
+
+if queue_on_teleport then
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ricardojrgavin-afk/BrainrotPolice/main/src/init.lua"))()')
+end
